c1cf1a951e0dc7f53c39267e625d18fe79e67588
Hraday04 <hraday04@gmail.com>
2024-04-20 19:18:57 +0530

    flowbite

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,11 @@
-from app import app,db,User,login_user,logout_user,current_user,Files
+from app import app,db,User,login_user,logout_user,current_user,Files,login_required
 from flask import render_template,redirect,request,url_for,jsonify,send_file
 import uuid as uuid
 from firebase_admin import credentials, initialize_app, storage
-<<<<<<< HEAD
 
-cred = credentials.Certificate("C:/Users/DELL/Documents/ThunderDocs/important/thunderdocs-52311-d7e8d2d32861.json")
-=======
+
 cred = credentials.Certificate("C:/Users/vishw/ThunderDocs/important/thunderdocs-52311-d7e8d2d32861.json")
->>>>>>> 20511cb01e6da8ef1c74e7ae74da1a5693cb1db4
+
 initialize_app(cred, {'storageBucket': 'thunderdocs-52311.appspot.com'})
 
 @app.route('/register',methods=['GET','POST'])
@@ -48,6 +46,7 @@ def logout():
 
 @app.route('/')
 @app.route('/home')
+@login_required
 def index():
     files=Files.query.filter_by(user_id=current_user.id).all()
     if files:
@@ -68,7 +67,7 @@ def upload():
         )
         url = blob.public_url
         file=Files(
-            file_name=file.filename
+            file_name=file.filename,
             file_nameDb=file.filename,
             file_path = url,
             user_id=user_id,
@@ -77,13 +76,13 @@ def upload():
         db.session.add(file)
         db.session.commit()
         return jsonify({"message":"File uploaded successfully"})
+
 @app.route('/file/download/raw/<filename>')
 def downloadFile(fileId):
     file=Files.query.filter_by(id=fileId).first()
     if file:
-            
-            bucket = storage.bucket()
-            blob = bucket.blob(file_name)
+        bucket = storage.bucket()
+        blob = bucket.blob(file.file_name)
         return send_file(file.file_path,as_attachment=True)
     
     return jsonify({"message":"File not found"})
